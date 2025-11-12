665b21462d69981ce2ba339ddf36c194ef90e537
doraemon-kub <natthawut.sag@spumail.net>
2025-11-12 21:20:58 +0700

    cancle x

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import Login from "./pages/Login";
 
 function App() {
 
-    const [token, setToken] = useState('x');   
+    const [token, setToken] = useState('');   
     const [role, setRole] = useState('');
 
     const [products, setProducts] = useState([]);
